8f6e909cc852d204688b6d7920d145d693714942
Tony <1710583392@qq.com>
2018-08-03 18:41:23 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/admin/coolweather/gson/Province.java
@@ -0,0 +1,39 @@
+package com.example.admin.coolweather.gson;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Admin on 2018/8/3.
+ */
+
+public class Province extends DataSupport {
+
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public int getId() {
+
+        return id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+}
